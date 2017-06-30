e6a9cc5f7d51d4770a5a2db77dfba7cb23bcc8cf
Alejandro Buendia <alejandrobuendia@dyn-160-39-130-18.dyn.columbia.edu>
2017-06-30 03:24:22 -0400

    Revised code for W_{p+2}: first draft of basis computation.

---
+++ Ramsey Number/p_plus_two_revised/gen_basis.m
@@ -0,0 +1,96 @@
+% generate_basis(): basis for all unlabelled graphs with at least one K_p
+% (for action of a_p on W_{p+2})
+% Input: dimension of space p
+% Output: basis
+function result = gen_basis(p)
+mat_basis = {};
+valid_tuples = valid_tuples(p);
+mixed_tuples = mixed_tuples(p);
+% convert valid_tuples to matrices, and add to basis
+for i = 1:length(valid_tuples)
+    mat_basis{end+1} = tuple_to_matrix(valid_tuples{i});
+end
+% process mixed_tuples
+mixed_mat = ind_mixed_tuples(mixed_tuples);
+for i = 1:length(mixed_mat)
+    mat_basis{end+1} = mixed_mat{i};
+end
+result = mat_basis;
+end
+
+% If R has one K_p^{red} (in general) -- generate tuples (a,b,c,d) with:
+%   a + b + c + d = p
+%   b >= c
+%   a + b <= p - 2
+% NOTE: Includes single edge case where a + b = p - 2, which has two
+% K_p^{red}, but no duplicates.
+% Set of tuples are represented once and do not need more processing.
+function result = valid_tuples(p)
+valid_tuples = {};
+for x = 0:1
+    for a = p:-1:0
+        for b = (p-a-2):-1:0
+            for c = min(p-a-b,b):-1:0
+                for y = 0:1
+                    valid_tuples{end+1} = [x,a,b,c,p-a-b-c,y];
+                end
+            end
+        end
+    end
+end
+result = valid_tuples;
+end
+
+% If R has more than one K_p^{red} -- generate tuples (a,b,c,d) with:
+%   a + b + c + d = p
+%   b >= c
+%   a + b >= p - 1
+% NOTE: Set of tuples generated need to be processed further to eliminate 
+% duplicates.
+function result = mixed_tuples(p)
+mixed_tuples = {};
+for x = 0:1
+    for a = p:-1:0
+        for b = (p-a):-1:(p-a-1)
+            for c = min(p-a-b,b):-1:0
+                for y = 0:1
+                    mixed_tuples{end+1} = [x,a,b,c,p-a-b-c,y];
+                end
+            end
+        end
+    end
+end
+result = mixed_tuples;
+end
+
+function result = ind_mixed_tuples(mixed_tuples)
+mat_list = {};
+for i = 1:length(mixed_tuples)
+   cur_mat = tuple_to_matrix(mixed_tuples{i});
+   permutations = find_permutations(cur_mat);
+   for j = 1:length(permutations)
+       mat_list{end+1} = normalize_matrix(permutations{j});
+   end
+end
+% take the unique matrices in the list by converting to strings
+str_list = unique(cellfun(@mat2str,mat_list,'UniformOutput',false));
+mat_list = cellfun(@eval,str_list,'UniformOutput',false);
+result = mat_list;
+end
+
+function result = find_permutations(init_mat)
+p = length(init_mat) - 2;
+permutations = {};
+for i = (p+2):-1:1
+    for j = (p+1):-1:1
+        if i ~= p+1
+            init_mat(:,[p+2,i]) = init_mat(:,[i,p+2]);
+            init_mat([p+2,i],:) = init_mat([i,p+2],:);
+            init_mat(:,[p+1,j]) = init_mat(:,[j,p+1]);
+            init_mat([p+1,j],:) = init_mat([j,p+1],:);
+            permutations{end+1} = init_mat;
+        end
+    end
+end
+result = permutations;
+end
---
+++ Ramsey Number/p_plus_two_revised/tuple_to_matrix.m
@@ -0,0 +1,42 @@
+% tuple_to_matrix(): generates normalized adjacency matrix from given 6-tuple
+% Input: tuple [x = 0/1, a, b, c, d, y = 0/1] where
+%   x = 0/1 (blue or red K_p in upper left corner)
+%   (a,b,c,d) generated in valid_tuples() and mixed_tuples()
+%   y = 0/1 is (p+1,p+2)-th entry (red or blue edge between two separate
+%       vertices
+% Output: matrix
+function result = tuple_to_matrix(tuple)
+x = tuple(1); y = tuple(6);
+a = tuple(2); b = tuple(3); c = tuple(4); d = tuple(5);
+bound_1 = a + b; bound_2 = a + b + c; p = a + b + c + d;
+matrix = [];
+% set K_p in upper left corner
+if x == 0
+    matrix = zeros(p+2);
+else
+    matrix = ones(p+2) - eye(p+2);
+end
+% set a coordinates: (1,1)
+for i = 1:a
+    matrix(i,p+1) = 1;
+    matrix(i,p+2) = 1;
+end
+% set b coordinates: (1,0)
+for i = (a+1):bound_1
+    matrix(i,p+1) = 1;
+    matrix(i,p+2) = 0;
+end
+% set c coordinates: (0,1)
+for i = (bound_1+1):bound_2
+    matrix(i,p+1) = 0;
+    matrix(i,p+2) = 1;
+end
+% set d coordinates: (0,0)
+for i = (bound_2+1):p
+    matrix(i,p+1) = 0;
+    matrix(i,p+2) = 0;
+end
+% set y value and make symmetric
+matrix(p+1,p+2) = y;
+result = triu(matrix) + triu(matrix)';
+end
