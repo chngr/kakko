1ad84a37ab6009684429fafc4a20b92b8c54065d
Alejandro Buendia <alb2281@columbia.edu>
2017-07-17 15:24:36 -0400

    GAP file i/o.

--- RamseyNumber/p_plus_2/mat_to_txt.m
+++ RamseyNumber/p_plus_2/mat_to_txt.m
@@ -1,15 +1,19 @@
 % mat_to_txt(): writes collection of matrices to text file
 % Input: mat_set -- cell array of matrices
+%        txt_name -- name of text file
+%        p -- dimension of K_p
 % Output: .txt file with mat_set printed
 %         saved in kakko/RamseyNumber/classification directory
 % NOTE: Printed in Python-friendly syntax for use with eval() function. 
-function mat_to_txt(mat_set,txt_name)
+function mat_to_txt(mat_set,txt_name,p)
 % find destination directory relatively
 cur_dir = cd;
 dest_name = fullfile(cur_dir,'..','classification',txt_name);
 % write file
 format long
 fid = fopen(dest_name,'w');
+mat_list = strcat("mat_",int2str(p),"_plus_2 := ");
+fprintf(fid,mat_list);
 fprintf(fid,'[');
 for i = 1:length(mat_set) % loop through elements in mat_set
     fprintf(fid,'[');
@@ -24,14 +28,15 @@ for i = 1:length(mat_set) % loop through elements in mat_set
         end
         fprintf(fid,']');
         if j ~= length(mat_set{i})
-            fprintf(fid,',\n');
+            fprintf(fid,',');
         end
     end
     fprintf(fid,']');
     if i ~= length(mat_set)
-        fprintf(fid,',\n\n');
+        fprintf(fid,',');
     end
 end
 fprintf(fid,']');
+fprintf(fid,';');
 fclose(fid);
 end
--- RamseyNumber/p_plus_2/overall_function.m
+++ RamseyNumber/p_plus_2/overall_function.m
@@ -15,8 +15,8 @@ groups = grouping_basis(p,basis,map);
 gen_mat = {E,F};
 gen_names = {'E','F'};
 % write E and F to .txt file
-gen_file_name = strcat(mat2str(p),'_plus_2_gen.txt');
-mat_to_txt(gen_mat,gen_file_name);
+gen_file_name = strcat(mat2str(p),'_plus_2_gen.g');
+mat_to_txt(gen_mat,gen_file_name,p);
 %{
 % computes dimension and basis of generated Lie algebra 
 [dim,result_basis] = bracket_operation(gen_mat,gen_names);
