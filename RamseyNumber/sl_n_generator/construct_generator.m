559e975cf55be38c5c32bf2f27acc66664d3e742
jw3479 <jwanabust@yahoo.com>
2017-07-24 11:57:12 -0400

    construct_gen

--- RamseyNumber/sl_n_generator/construct_generator.m
+++ RamseyNumber/sl_n_generator/construct_generator.m
@@ -3,49 +3,40 @@
 % Output: 
 
 function [E,F] = construct_generator(max_val,partition_arr)
+whole_size = sum(partition_arr);
 E_block_mat = {};
 F_block_mat = {};
-num_blocks = length(partition_arr);
+num_partitions = length(partition_arr);
 
 % get all blocks
-for i = 1:num_blocks            % height coord of block
-    for j = 1:num_blocks   % length corrd of block
-        block_height = parition_arr(i);
+for i = 1:num_partitions            % height coord of block
+    for j = 1:num_partitions   % length corrd of block
+        block_height = partition_arr(i);
         block_length = partition_arr(j);
-        if j == i+1 % not subdiag block
+        if j == i+1 % superdiag
             temp_cell = block_gen(block_height,block_length,max_val);
             E_block_mat{end+1} = temp_cell{1};
-        else
-            E_block_mat{end+1} = zeros(block_height,block_length);
+            F_block_mat{end+1} = temp_cell{2};
         end
     end
 end
 
-for i = 1:num_blocks            % height coord of block
-    for j = 1:num_blocks   % length corrd of block
-        block_height = parition_arr(i);
-        block_length = partition_arr(j);
-        if j == i-1 % not subdiag block
-            temp_cell = block_gen(block_height,block_length,max_val);
-            E_block_mat{end+1} = temp_cell{2};
-        else
-            F_block_mat{end+1} = zeros(block_height,block_length);
-        end
-    end
-end
-E = blocks_2_matrix(E_block_mat);
-F = blocks_2_matrix(F_block_mat);
+E = blocks_2_matrix(E_block_mat,num_partitions,whole_size);
+F = blocks_2_matrix(F_block_mat,num_partitions,whole_size);
 end
 
 
-function result_mat = blocks_2_matrix(block_mat)
-result_mat = 0;
-for i = 1: length(block_mat)
+function result_mat = blocks_2_matrix(block_mat,num_partitions,whole_size, gen)
+result_mat = zeros(whole_size);
+for i = 1: length(num_partitions)
     % concatenate each block
-    cur_row = block_mat{1};
-    for j = 1: length(block_mat)
-        cur_block = block_mat{(i-1)*num_blocks + j};
-        cur_row = [cur_row, cur_block];
+    cur_row = 0;
+    for j = 1: length(num_partitions)
+        if gen == 'E' && j == i+1
+            cur_block = block_mat{i};
+        else gen == 'F' && j == i-1
+            cur_block = block_mat{i};
+        end
     end
     if i == 1
         result_mat = cur_row;
